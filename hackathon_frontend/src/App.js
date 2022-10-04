8679278f66138e0c98cbe05c61a7a62fe9b18bb6
shantanussalvi <shantanussalvi@gmail.com>
2022-10-05 00:52:50 +0530

    organizer

--- hackathon_frontend/src/App.js
+++ hackathon_frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Organizer from "./Modules/Organizer";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/organize" element={<Home />} />
+          <Route path="/organize" element={<Organizer />} />
           <Route path="/trending" element={<Home />} />
           <Route path="/categories" element={<Home />} />
         </Routes>
