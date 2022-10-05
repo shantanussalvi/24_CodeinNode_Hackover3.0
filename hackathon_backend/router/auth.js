8363d712f12b1d08f08b2364d76bf1db03f56b80
shantanussalvi <shantanussalvi@gmail.com>
2022-10-05 06:55:37 +0530

    backend

---
+++ hackathon_backend/router/auth.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+// Router for backend
+const router = express.Router();
+
+require('../db/conn');
+const User = require('../models/userSchema');
+
+// Using async-await
+router.post('/register', async (req, res) => {
+    // get the data sent by user
+    const { name, email, phone, idProof, password, cpassword } = req.body;
+
+    // Validation
+    if ( !name || !email || !phone || !idProof || !password ||!cpassword ) {
+        // 422 - Unprocessable entity
+        return res.status(422).json({ error: "Please fill all the fields" });
+    }
+    try {
+        // check if email is present in the database
+        // Await - waits for promise to return
+        // find one returns entire document if email exists
+        const userExist = await User.findOne({ email: email });
+
+        if (userExist) {
+            return res.status(422).json({ error: "Email already exists" });
+        } else if (password !== cpassword) {
+            return res.status(422).json({ error: "Password and confirm password not matching" });
+        } else {
+            const user = new User({ name, email, phone, idProof, password, cpassword });
+
+            await user.save();
+
+            res.status(201).json({ message: "user registered successfully" });
+            // As this is try-catch if error occurs it will execute catch
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// login route
+router.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Validation
+        if (!email || !password) {
+            // 422 - Unprocessable entity
+            return res.status(422).json({ error: "Please fill all the fields" });
+        }
+
+        const userLogin = await User.findOne({ email: email });
+        console.log(userLogin);
+
+        if (userLogin) {
+            const isMatch = await bcrypt.compare(password, userLogin.password);
+
+            // jwt(json web token) - unique id
+            const token = await userLogin.generateAuthToken();
+            console.log(token);
+
+            // Creates a cookie which expires after 30 days
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now()+25892000000),
+                httpOnly: true
+            });
+
+            if (!isMatch) {
+                // 400 - Bad request
+                res.status(400).json({ error: 'Invalid credentials' });
+            } else {
+                res.json({ message: 'user signed in sucessfully' });
+            }
+        } else {
+            res.status(400).json({ error: 'Invalid credentials' });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+module.exports = router;
